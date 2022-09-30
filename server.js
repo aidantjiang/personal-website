82df669639a5f98a3b29bc2b74e32f3694fe3d1d
Aidan Jiang <aidanjiang9@gmail.com>
2022-09-30 11:34:37 -0600

    reordered files

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('static'));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/index.html');
+    res.sendFile(__dirname + '/static/html/home.html');
 })
 
 app.post('/', (req, res) => {
