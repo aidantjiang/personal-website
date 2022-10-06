7cb425132a2e44d2e1a3874b87acd3380c783c10
Aidan Jiang <aidanjiang9@gmail.com>
2022-10-06 12:42:23 -0600

    wip, directs instantaneousonly atm

--- static/js/home.js
+++ static/js/home.js
@@ -1,2 +1,11 @@
 const width = screen.width;
 const height = screen.height;
+
+let links = document.getElementsByClassName("link");
+console.log(links[0]);
+for (let i = 0; i < links.length; i++) {
+    console.log(`delaying link ${i}`)
+    window.setTimeout(function() {
+        location.href = document.getElementById("social").getElementsByTagName("a")[i].href;
+    }, 2000);
+}
