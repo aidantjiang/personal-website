cbb0184be9a2ce5937db0ff3596b8d0a575c6106
Aidan Jiang <aidanjiang9@gmail.com>
2022-10-07 22:22:24 -0600

    fixed instant redirect and remade text on main screen

--- static/js/home.js
+++ static/js/home.js
@@ -5,7 +5,9 @@ let links = document.getElementsByClassName("link");
 console.log(links[0]);
 for (let i = 0; i < links.length; i++) {
     console.log(`delaying link ${i}`)
-    window.setTimeout(function() {
-        location.href = document.getElementById("social").getElementsByTagName("a")[i].href;
-    }, 2000);
+    links[i].addEventListener("click", () => {
+        window.setTimeout(function() {
+            location.href = document.getElementById("social").getElementsByTagName("a")[i].href;
+        }, 2000);
+    })
 }
