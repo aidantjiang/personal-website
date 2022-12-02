13a1b3b3e540bbc58e57f8e034be675e68966e4e
Aidan Jiang <aidanjiang9@gmail.com>
2022-12-02 11:40:36 -0700

    website ready for deploymont (horrible on mobile atm)

--- static/js/code.js
+++ static/js/code.js
@@ -28,31 +28,36 @@ const headerText = [
 const bodyText = [
     "5-week Colorado School of Mines Summer Program that allows students to create and direct their own projects. Teaches technical skills, collaborative work, and communication. <br><br> During my summer at this internship, my group decided to make a static website that educates on deforestation. Written with HTML, CSS, and JavaScript, my main role in the project was to create an interactive map of the world using D3.js, a data visualizing JavaScript package. <br><br> A link to our code can be found below.",
     "Taken during quarantine, Harvard's Online CS50 Course provides valuable experience in every area of computer science. Covering (although lightly) C, Python, Algorithms, Big O Notation, Flask, and essential skills that every developer needs, CS50 provides a great baseline.",
-    "",
-    "",
-    "",
-    ""
+    "A local  team based in Longmont, Colorado, Team 1619 (Up-A-Creek Robotics) is a FIRST Robotics team that competes at a world class level. Completely student-directed, Team 1619 is a closely community of high school student who fabricate, design, and code an robot made of pure steel. <br><br> I'm personally a app development subteam lead. I direct the part of the organizaiton that creates a fullstack app in React Native (Typescript) designed to scout other teams at conpetitions.",
+    "Coded in raw HTML, CSS (SCSS), and JavaScript, I hand-coded this entire website. Although some effects have been refactored from an internet source, every main feature of this website is made with blood, sweat, and tears.",
+    "An in-progress community project to advance my high school (Niwot High School) and their tutoring program, me and team of three other high schoolers and currently working to create an app for Niwot High School's tutoring program.",
+    "I got a 5 when I took this AP class last year. This is really more of a placeholder than an achievement."
 ];
 
 const linkText = [
     {
-
+        link: "https://cstart.mines.edu/c4g/",
+        title: "// mines internship"
     },
     {
-
+        link: "https://pll.harvard.edu/course/cs50-introduction-computer-science?delta=0",
+        title: "// cs50"
     },
     {
-
+        link: "https://www.team1619.org/",
+        title: "// up-a-creek robotics"
     },
     {
         link: "https://github.com/aidantjiang/personal-website",
-        
+        title: "// personal website code"
     },
     {
-
+        link: "",
+        title: "// no code available"
     },
     {
-
+        link: "https://apstudents.collegeboard.org/courses/ap-computer-science-a",
+        title: "// ap csa"
     },
 ];
 
@@ -70,7 +75,8 @@ const flags = [true, true, true, true, true, true];
 window.addEventListener("load", () => {
     const infobox = document.getElementById("info-wrapper");
     const infoheader = document.getElementById("info-header");
-    const infobody = document.getElementById("info-body")
+    const infobody = document.getElementById("info-body");
+    const infolink = document.getElementById("info-link");
     let switchNum;
 
     for (let i = 0; i < logos.length; i++) {
@@ -84,6 +90,8 @@ window.addEventListener("load", () => {
 
             infoheader.innerHTML = headerText[switchNum];
             infobody.innerHTML = bodyText[switchNum];
+            infolink.innerHTML = linkText[switchNum].title;
+            infolink.href = linkText[switchNum].link;
 
             window.setTimeout(() => {
 
